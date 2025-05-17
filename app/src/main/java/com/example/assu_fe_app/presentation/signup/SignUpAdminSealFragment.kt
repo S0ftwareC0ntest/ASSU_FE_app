69df5148ef2746a922dd028819bad2f6684a9626
2ghrms <aa020228@gmail.com>
2025-05-18 05:26:40 +0900

    [FEAT/#11] 회원가입 프래그먼트 뼈대 작성

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/signup/SignUpAdminSealFragment.kt
@@ -0,0 +1,14 @@
+package com.example.assu_fe_app.presentation.signup
+
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentSignUpAdminSealBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class SignUpAdminSealFragment : BaseFragment<FragmentSignUpAdminSealBinding>(R.layout.fragment_sign_up_admin_seal) {
+    override fun initObserver() {
+    }
+
+    override fun initView() {
+    }
+
+}
---
+++ app/src/main/java/com/example/assu_fe_app/presentation/signup/SignUpUserSchoolFragment.kt
@@ -0,0 +1,14 @@
+package com.example.assu_fe_app.presentation.signup
+
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentSignUpUserSchoolBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class SignUpUserSchoolFragment : BaseFragment<FragmentSignUpUserSchoolBinding>(R.layout.fragment_sign_up_user_school) {
+    override fun initObserver() {
+    }
+
+    override fun initView() {
+    }
+
+}
