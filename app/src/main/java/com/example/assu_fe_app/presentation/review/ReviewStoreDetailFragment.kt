28847bc84cec0ef199b0ed809c71d7bb779d78e7
isumin <leesumin0526@gmail.com>
2025-05-17 17:02:09 +0900

    [FEAT/#9] - 가게 선택 리뷰 전체보기 UI 구현 완료

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/review/ReviewStoreDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.assu_fe_app.presentation.review
+
+import android.content.Intent
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentLoactionBinding
+import com.example.assu_fe_app.databinding.FragmentReviewStoreDetailBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class ReviewStoreDetailFragment :
+    BaseFragment<FragmentReviewStoreDetailBinding>(R.layout.fragment_review_store_detail) {
+
+    override fun initView() {
+    }
+
+    override fun initObserver() {
+    }
+
+}
