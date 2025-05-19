90220001a2909f2394087555504d791fa1e05295
isumin <leesumin0526@gmail.com>
2025-05-20 03:12:02 +0900

    [FIX/#9] 파일명에 따라 binding 변수명 수정

--- app/src/main/java/com/example/assu_fe_app/presentation/common/location/LocationItemUserFragment.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/location/LocationItemUserFragment.kt
@@ -1,11 +1,11 @@
 package com.example.assu_fe_app.presentation.common.location
 
 import com.example.assu_fe_app.R
-import com.example.assu_fe_app.databinding.FragmentLocationItemBinding
+import com.example.assu_fe_app.databinding.FragmentLocationItemUserBinding
 import com.example.assu_fe_app.presentation.base.BaseFragment
 
 class LocationItemUserFragment :
-    BaseFragment<FragmentLocationItemBinding>(R.layout.fragment_location_item_user) {
+    BaseFragment<FragmentLocationItemUserBinding>(R.layout.fragment_location_item_user) {
     override fun initObserver() {}
 
     override fun initView() {}
