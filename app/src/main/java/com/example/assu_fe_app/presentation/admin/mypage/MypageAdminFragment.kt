c0479c22a8ca50b1688fa4da87f361f41e3abb2b
eeeeeaaan <wjdwjddla73@naver.com>
2025-05-20 19:50:00 +0900

    [MOD/#16] 관리자/제휴 업체 계정관리 화면

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/admin/mypage/MypageAdminFragment.kt
@@ -0,0 +1,19 @@
+package com.example.assu_fe_app.presentation.admin.mypage
+
+
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentMypageAdminBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+
+class MypageAdminFragment : BaseFragment<FragmentMypageAdminBinding>(R.layout.fragment_mypage_admin) {
+    override fun initView(){
+
+    }
+
+    override fun initObserver() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/assu_fe_app/presentation/partner/mypage/MypagePartnerFragment.kt
@@ -0,0 +1,14 @@
+package com.example.assu_fe_app.presentation.partner.mypage
+
+import android.os.Bundle
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentMypagePartnerBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class MypagePartnerFragment : BaseFragment<FragmentMypagePartnerBinding>(R.layout.fragment_mypage_partner) {
+
+    override fun initView(){}
+
+    override fun initObserver(){}
+
+}
