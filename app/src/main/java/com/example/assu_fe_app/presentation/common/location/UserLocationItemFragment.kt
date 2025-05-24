e314f173151b413f9790ea3cb3e961ac33441bd1
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-05-24 23:17:22 +0900

    [FEAT/#19] 관리자, 업체용 지도 item ui 구현

--- app/src/main/java/com/example/assu_fe_app/presentation/common/location/UserLocationItemFragment.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/location/UserLocationItemFragment.kt
@@ -1,11 +1,11 @@
 package com.example.assu_fe_app.presentation.common.location
 
 import com.example.assu_fe_app.R
-import com.example.assu_fe_app.databinding.FragmentLocationItemUserBinding
+import com.example.assu_fe_app.databinding.ItemUserLocationBinding
 import com.example.assu_fe_app.presentation.base.BaseFragment
 
-class LocationItemUserFragment :
-    BaseFragment<FragmentLocationItemUserBinding>(R.layout.fragment_location_item_user) {
+class UserLocationItemFragment :
+    BaseFragment<ItemUserLocationBinding>(R.layout.item_user_location) {
     override fun initObserver() {}
 
     override fun initView() {}
