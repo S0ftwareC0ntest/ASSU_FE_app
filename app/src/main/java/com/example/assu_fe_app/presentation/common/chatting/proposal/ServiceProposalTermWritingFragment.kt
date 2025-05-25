5a896fbf6dd00c27a7e5d045cc13736f80d8a4fd
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-05-25 22:22:29 +0900

    [FIX/#19] 에러 처리

--- app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/proposal/ServiceProposalTermWritingFragment.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/proposal/ServiceProposalTermWritingFragment.kt
@@ -1,12 +1,12 @@
 package com.example.assu_fe_app.presentation.common.chatting.proposal
 
 import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentServiceProposalTermWritingBinding
 import com.example.assu_fe_app.databinding.ItemServiceProposalSetBinding
 import com.example.assu_fe_app.presentation.base.BaseFragment
 
-class ServiceProposalSetItemFragment :
-    BaseFragment<ItemServiceProposalSetBinding>(R.layout.item_service_proposal_set) {
-
+class ServiceProposalTermWritingFragment :
+    BaseFragment<FragmentServiceProposalTermWritingBinding>(R.layout.fragment_service_proposal_term_writing) {
     override fun initObserver() {}
 
     override fun initView() {}
