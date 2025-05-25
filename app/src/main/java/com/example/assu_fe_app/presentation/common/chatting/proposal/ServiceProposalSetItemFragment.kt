6a74b7ab1aa4818b850c5b319da1348f4598c229
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-05-25 17:40:22 +0900

    [FEAT/#19] 제휴 제안서 보기, 제휴 제안서 작성하기 view 구현

---
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/proposal/ServiceProposalSetItemFragment.kt
@@ -0,0 +1,14 @@
+package com.example.assu_fe_app.presentation.common.chatting.proposal
+
+import com.example.assu_fe_app.R
+import com.example.assu_fe_app.databinding.FragmentServiceProposalBinding
+import com.example.assu_fe_app.databinding.ItemServiceProposalSetBinding
+import com.example.assu_fe_app.presentation.base.BaseFragment
+
+class ServiceProposalSetItemFragment :
+    BaseFragment<ItemServiceProposalSetBinding>(R.layout.item_service_proposal_set) {
+
+    override fun initObserver() {}
+
+    override fun initView() {}
+}
