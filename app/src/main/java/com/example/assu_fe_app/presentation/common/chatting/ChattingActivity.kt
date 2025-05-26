ac13296e2ebab3b36b683807ac6e6024091b2109
BAEK0111 <baekjw111@gmail.com>
2025-05-26 19:50:39 +0900

    feat #17: - ChattingActivity -> chattingListFragment 수정

--- app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/ChattingActivity.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/ChattingActivity.kt
@@ -1,15 +1,17 @@
 package com.example.assu_fe_app.presentation.common.chatting
 
 import android.content.Context
+import android.content.Intent
 import android.widget.ImageView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assu_fe_app.R
 import com.example.assu_fe_app.databinding.ActivityChattingBinding
+import com.example.assu_fe_app.presentation.admin.AdminMainActivity
 import com.example.assu_fe_app.presentation.base.BaseActivity
 import com.example.assu_fe_app.presentation.common.chatting.adapter.ChattingChatListAdapter
-
+import com.example.assu_fe_app.presentation.user.UserMainActivity
 
 
 class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity_chatting) {
@@ -28,10 +30,7 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
         }
 
         binding.ivChattingBack.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.chatting_fragment_container,ChattingListFragment())
-                .addToBackStack(null)
-                .commit()
+            navigateToChatting()
         }
 
     }
@@ -44,5 +43,14 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
         return (this * context.resources.displayMetrics.density).toInt()
     }
 
-
+    private fun navigateToChatting() {
+        val intent = Intent(this, AdminMainActivity::class.java).apply {
+            // 기존 Task 스택 위로 올라가서 중복 생성 방지
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            // BottomNavigationView에 전달할 목적지 ID
+            putExtra("nav_dest_id", R.id.adminChattingFragment)
+        }
+        startActivity(intent)
+        finish() // FinishReviewActivity 종료
+    }
 }
