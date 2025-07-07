87dc7657e8d8b88ae65777990a49c7f526b4dc1c
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-07-07 22:31:38 +0900

    [FIX/#33] 지도 아이템 이동 구현   - 문의하기: 새로운 채팅방 생성되고 그 채팅 화면으로 이동 (message) 도착 메시지: '문의하기' 버튼을 통해 이동했습니다.)   - 제휴 계약서 보기: 기존의 둘 간의 채팅 화면으로 이동하고 제휴 계약서 모달 띄움   (message) 도착 메시지: '제휴 계약서 보기' 버튼을 통해 이동했습니다.)

--- app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/ChattingActivity.kt
+++ app/src/main/java/com/example/assu_fe_app/presentation/common/chatting/ChattingActivity.kt
@@ -34,6 +34,12 @@ class ChattingActivity : BaseActivity<ActivityChattingBinding>(R.layout.activity
             insets
         }
 
+        // 지도에서 어느 버튼을 통해 왔는지 알려주는 메시지를 받는 코드입니다.
+        // 문의하기: 채팅방 생성되고 해당 방으로 들어가지게
+        // 제휴 계약서 보기: 채팅 창으로 이동해서 제휴 계약서 모달 띄우기
+        val entryMessage = intent.getStringExtra("entryMessage")
+        Log.d("ChattingActivity", "도착 메시지: $entryMessage")
+
         binding.ivChattingBack.setOnClickListener {
             navigateToChatting()
         }
